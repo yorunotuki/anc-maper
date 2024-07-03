73de39da9b1e5ebdfef106984a161e3ae5986198
yorunotuki <r.y.o627b@gmail.com>
2024-07-03 19:06:19 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anc-maper",
+    version="0.0.1",
+    author="ryo komatsu",
+    author_email="r.y.o627b@gmail.com",
+    description="Know the old names of prefectures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yorunotuki/anc-maper",
+    project_urls={
+        "Bug Tracker": "https://github.com/yorunotuki/anc-maper",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['anc-maper'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.12",
+    entry_points = {
+        'console_scripts': [
+            'anc-maper = anc-maper:main'
+        ]
+    },
+)
